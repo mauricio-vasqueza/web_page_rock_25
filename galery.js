2170a2558e92ddbec065f661215607d017cee4b4
mauricio-vasqueza <9405384@alumnos.ufv.es>
2026-01-07 02:21:04 +0100

    hasta ahora la galeria esta a medias, faltan 30 imagenes.

---
+++ galery.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+  console.log("NanoGallery inicializando ✅");
+
+  $("#nanogallery").nanogallery2({
+
+    // ---- ORIGEN: HTML (<a>) ----
+    itemsBaseURL: "",
+
+    // ---- LAYOUT ----
+    thumbnailHeight: 200,
+    thumbnailWidth: "auto",
+    galleryMaxRows: 0,
+    thumbnailAlignment: "fillWidth",
+
+    // ---- ESPACIADO ----
+    thumbnailL1GutterWidth: 15,
+    thumbnailL1GutterHeight: 15,
+
+    // ---- ANIMACIONES ----
+    thumbnailDisplayTransition: "flipUp",
+    thumbnailDisplayTransitionDuration: 400,
+    thumbnailDisplayInterval: 150,
+    thumbnailDisplayOrder: "rowByRow",
+
+    thumbnailHoverEffect2: "toolsSlideUp|labelSlideDown",
+
+    // ---- TOOLS ----
+    thumbnailToolbarImage: {
+      bottomRight: "zoom,fullscreen,download"
+    },
+
+    // ---- OTROS ----
+    locationHash: false
+  });
+
+});
